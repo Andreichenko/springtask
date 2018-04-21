4bcefcf7d4ec06e86fc6905b63b414070e4f66f2
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-22 00:46:18 +0300

    add Application

--- src/main/java/com/frei/test/springtask/StartApplication.java
+++ src/main/java/com/frei/test/springtask/StartApplication.java
@@ -1,9 +1,21 @@
 package com.frei.test.springtask;
 
 
+import com.frei.test.springtask.interfaces.CarRepository;
+import com.frei.test.springtask.objects.Car;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+import java.util.stream.Stream;
 
 @EnableResourceServer
 @SpringBootApplication
@@ -11,5 +23,33 @@ public class StartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(CarRepository carRepository){
+        return args -> {
+            Stream.of("Mercedes", "BMW", "Porsche", "Lamborgini" , "Veron",
+                    "AMG BigЭ", "Tiple marge", "Impal fire").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                carRepository.save(car);
+            });
+            carRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    @SuppressWarnings("unchecked")
+    public FilterRegistrationBean simpleCarFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", configuration);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
