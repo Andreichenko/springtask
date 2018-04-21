8264fb0f779e76ea099395792f33cd14870cec44
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-22 00:08:26 +0300

    fix change and extends interface

--- src/main/java/com/frei/test/springtask/interfaces/CarRepository.java
+++ src/main/java/com/frei/test/springtask/interfaces/CarRepository.java
@@ -1,9 +1,13 @@
 package com.frei.test.springtask.interfaces;
 
 
+import com.frei.test.springtask.objects.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-public interface CarRepository {
+@CrossOrigin(origins = "http://localhost:4200")
+public interface CarRepository extends JpaRepository<Car, Long>{
 
 }
