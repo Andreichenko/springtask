9b65ac470b18f920632f8e85a50c675a90ea4d25
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-22 00:32:40 +0300

    add test

--- src/main/java/com/frei/test/springtask/controllers/CarController.java
+++ src/main/java/com/frei/test/springtask/controllers/CarController.java
@@ -1,10 +1,33 @@
 package com.frei.test.springtask.controllers;
 
 
+import com.frei.test.springtask.interfaces.CarRepository;
+import com.frei.test.springtask.objects.Car;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 public class CarController {
 
 
+    private CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Collection<Car> cars(){
+        return carRepository.findAll().stream()
+                .filter(this::isCars)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCars(Car car){
+        return !car.getName().equals("AMG Big") &&
+                !car.getName().equals("Tiple marge") &&
+                !car.getName().equals("Impal fire");
+    }
 }
